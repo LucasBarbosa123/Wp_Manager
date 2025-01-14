8462f665990126ebc695eaa65123a8e75f8815b1
LucasBarbosa123 <lucasobrab@yahoo.com>
2025-01-14 23:29:29 +0000

    Adicionar arquivos de projeto.

---
+++ WP_Manager/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+using System.Diagnostics;
+
+namespace WP_Manager.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return View("/Pages/Index.cshtml");
+        }
+
+        [HttpGet("/BackOffice")]
+        public IActionResult BackOffice()
+        {
+            //var users = dbContext.User.ToList();
+
+            //ViewBag.Users = users;
+
+            return View("/Pages/BackOffice.cshtml");
+        }
+    }
+}
---
+++ WP_Manager/Models/WpManagerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WP_Manager.Models;
+
+namespace Models
+{
+    public class WpManagerDbContext : DbContext
+    {
+        public WpManagerDbContext(DbContextOptions<WpManagerDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
